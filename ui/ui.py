6e13ae98322374fbe991a6b2b88c8ae19569d7e5
brandon jacobs <brandonjacobs@comcast.net>
2013-08-20 15:00:07 -0400

    Initial Commit

---
+++ ui/ui.py
@@ -0,0 +1,21 @@
+"""
+ui.py is  used as the starting point for the GIS GUI application. This file will initialize the window, and all the sub structure's
+that integrate into the window. It will allow the user to query locations from the db dump of data points, plot on a map from the location
+data and other required information.
+"""
+
+#import pyqt4
+
+
+def main():
+    """
+    Pass Main GUI information here
+    """
+    #Initialize GUI and Main application ehre
+
+
+
+
+
+if __name__ == '__main__':
+    main()
